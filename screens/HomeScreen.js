d4c5ff86eb65cf5b12571853591fd0f1c12fe141
cSScripter <parksolprojects@gmail.com>
2025-07-04 14:13:21 +0300

    Basic HomeScreen Functionality and StyleSheet

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,29 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, FlatList, StyleSheet } from 'react-native';
+import styles from '../styles/HomeScreen.styles';
+
+const dummyLists = [
+  { id: '1', title: 'Groceries' },
+  { id: '2', title: 'Work Tasks' },
+  { id: '3', title: 'Weekend Plans' },
+];
 
 function HomeScreen({ navigation }) {
-    return (
-        <View>
-            <TouchableOpacity onPress={() => navigation.navigate('ToDoList', { title: 'My List'})}>
-                <Text>Go to ToDo List</Text>
+    const renderItem = ({ item }) => (
+        <TouchableOpacity
+            style={styles.listItem}
+            onPress={() => navigation.navigate('ToDoList', { title: item.title })}
+            >
+                <Text style={styles.listTitle}>{item.title}</Text>
             </TouchableOpacity>
+    );
+
+    return (
+        <View style={styles.container}>
+            <FlatList
+                data={dummyLists}
+                keyExtractor={(item) => item.id}
+                renderItem={renderItem} />
         </View>
     );
 }
---
+++ styles/HomeScreen.styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    marginTop: 20,
+  },
+  listItem: {
+    backgroundColor: '#e6e6fa',
+    padding: 15,
+    marginBottom: 10,
+    borderRadius: 10,
+  },
+  listTitle: {
+    fontSize: 18,
+    color: '#333',
+  },
+});
+
+export default styles;
